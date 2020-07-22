211892e561e5f2d27441cde74080ca51fef62329
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-07-22 23:22:31 +0900

    Update view pug logics (#27)

--- src/lib/posts-handler.js
+++ src/lib/posts-handler.js
@@ -1,9 +1,13 @@
 'use strict';
+const pug = require('pug');
 
 function handle(req, res) {
     switch (req.method) {
         case 'GET':
-            res.end('hi');
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            res.end(pug.renderFile('src/views/posts.pug'));
             break;
         case 'POST':
             break;
