868a6e5b0a7f96dbe6a0c15d069ed86f73afacba
yosh1 <yoppy1125yoppy@gmail.com>
2020-09-19 12:00:00 +0900

    Add post func

--- src/lib/posts-handler.js
+++ src/lib/posts-handler.js
@@ -11,10 +11,31 @@ function handle(req, res) {
             res.end(pug.renderFile('./src/views/posts.pug'));
             break;
         case 'POST':
+            let body = [];
+            // data / endを受け取ったとき
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                // buffer ... バイナリを扱う。脆弱性があるのであとでリプレイス
+                // concat ... 配列の結合
+                const decoded = decodeURIComponent(body);
+                const content = decoded.split('content=')[1]; // key=valueのため
+                console.info('Posted: ' + content);
+                handleRedirectPosts(req, res);
+            })
             break;
         default:
             break;
     }
+}
+
+function handleRedirectPosts(req, res) {
+    // 303 - See Other
+    res.writeHead(303, {
+        'Location': '/posts'
+    });
+    res.end();
 }
 
 module.exports = {
